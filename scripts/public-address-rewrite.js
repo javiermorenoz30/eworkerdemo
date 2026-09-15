7f8769cb47430f2b7f8145bb2087c9afcbd498b8
javiermorenoz30 <84947673+javiermorenoz30@users.noreply.github.com>
2026-09-15 17:39:56 -0400

    chore: stage address rewrite logic

---
+++ scripts/public-address-rewrite.js
@@ -0,0 +1,2 @@
+export const spanishAddress = 'Calle Basilio Gil número 17 San Antonio, próximo a la avenida Pedro A Rivera, provincia de La Vega, República Dominicana'
+export const englishAddress = '17 Basilio Gil Street, San Antonio, near Pedro A Rivera Avenue, La Vega Province, Dominican Republic'
